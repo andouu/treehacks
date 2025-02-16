f11b36d783d746654132107a14d12c2ad437ad0b
Andrew Zhou <andrewzhou16@gmail.com>
2025-02-15 23:03:44 -0800

    Add: Chat formatting

--- src/app/(protected)/dashboard/stocks/[id]/analysis/Components/PromptWindow.tsx
+++ src/app/(protected)/dashboard/stocks/[id]/analysis/Components/PromptWindow.tsx
@@ -5,38 +5,69 @@ import { motion } from "motion/react";
 import clsx from "clsx";
 
 const PromptWindow = () => {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const { messages, input, handleInputChange, handleSubmit, status, stop } =
+    useChat();
 
   return (
     <div className="w-[530px] h-full flex flex-col justify-end resize-x border-l-2 border-neutral-100 overflow-hidden">
-      <div className="relative flex-1 overflow-y-auto overflow-x-hidden px-3 py-2">
+      <div className="relative h-full flex-1 overflow-hidden px-3 pt-5">
         {messages.length > 0 ? (
-          <div className="flex flex-col gap-5">
-            {messages.map((m) => (
-              <div key={m.id} className="flex gap-3">
-                {m.role === "assistant" && (
+          <div className="flex-1 h-full flex flex-col justify-end overflow-hidden">
+            <div className="flex flex-col gap-5 overflow-y-auto">
+              <motion.span
+                className="mt-auto text-center text-neutral-400 text-sm"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+              >
+                Beginning of conversation
+              </motion.span>
+              {messages.map((m) => (
+                <div key={m.id} className="flex gap-3">
+                  {m.role === "assistant" && (
+                    <div className="w-9 h-9 shrink-0 p-2 flex items-center justify-center rounded-full border border-neutral-100">
+                      <img src="/logo_purple.svg" />
+                    </div>
+                  )}
+                  <motion.div
+                    className={clsx(
+                      "flex-1 mt-1.5 whitespace-pre-wrap break-words",
+                      m.role === "user" ? "text-end pl-3" : "text-start pr-3"
+                    )}
+                    style={{
+                      transformOrigin: m.role === "user" ? "right" : "left",
+                    }}
+                    initial={{ opacity: 0, scale: 0.9 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                  >
+                    {m.content}
+                  </motion.div>
+                  {m.role === "user" && (
+                    <div className="w-9 h-9 shrink-0 p-2 flex items-center justify-center rounded-full border border-neutral-100">
+                      <img src="/orange_star.svg" />
+                    </div>
+                  )}
+                </div>
+              ))}
+              {status === "submitted" && (
+                <div className="flex gap-3">
                   <div className="w-9 h-9 shrink-0 p-2 flex items-center justify-center rounded-full border border-neutral-100">
                     <img src="/logo_purple.svg" />
                   </div>
-                )}
-                <motion.div
-                  className={clsx(
-                    "flex-1 mt-1.5 whitespace-pre-wrap break-words",
-                    m.role === "user" ? "text-end" : "text-start"
-                  )}
-                  style={{ transformOrigin: "left" }}
-                  initial={{ opacity: 0, scale: 0.9 }}
-                  animate={{ opacity: 1, scale: 1 }}
-                >
-                  {m.content}
-                </motion.div>
-                {m.role === "user" && (
-                  <div className="w-9 h-9 shrink-0 p-2 flex items-center justify-center rounded-full border border-neutral-100">
-                    <img src="/orange_star.svg" />
-                  </div>
-                )}
-              </div>
-            ))}
+                  <motion.div
+                    className={clsx(
+                      "flex-1 mt-1.5 whitespace-pre-wrap break-words text-start pr-3 text-neutral-400 italic"
+                    )}
+                    style={{
+                      transformOrigin: "left",
+                    }}
+                    initial={{ opacity: 0, scale: 0.9 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                  >
+                    Aaron is thinking...
+                  </motion.div>
+                </div>
+              )}
+            </div>
           </div>
         ) : (
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-2">
@@ -70,13 +101,14 @@ const PromptWindow = () => {
           <div className="flex justify-between items-end">
             <span className="text-sm text-neutral-500">{input.length}</span>
             <button
-              type="submit"
               className="w-10 h-10 flex items-center justify-center rounded-full bg-neutral-800 active:scale-90 transition-transform duration-150"
+              type="submit"
+              onClick={() => (status === "ready" ? undefined : stop())}
             >
-              {true ? (
-                <LoadingIndicator scale={0.175} fg="white" bg="darkgray" />
-              ) : (
+              {status === "ready" ? (
                 <HiArrowUp className="text-neutral-50 stroke-1" />
+              ) : (
+                <LoadingIndicator scale={0.175} fg="white" bg="darkgray" />
               )}
             </button>
           </div>
