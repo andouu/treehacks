d3b031a118c4a606f2555e176354274401d38b32
Andrew Zhou <andrewzhou16@gmail.com>
2025-02-16 02:51:21 -0800

    Add: Portfolio optimization

--- src/app/(protected)/dashboard/layout.tsx
+++ src/app/(protected)/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { HiBriefcase } from "react-icons/hi";
 import { HiCog6Tooth } from "react-icons/hi2";
 
 export default function DashboardLayout({
@@ -12,12 +13,20 @@ export default function DashboardLayout({
         <Link href="/dashboard">
           <img src="/brand.svg" className="h-5 object-contain" />
         </Link>
-        <Link
-          href="/dashboard/settings"
-          className="border-2 p-1 rounded-full active:scale-90 transition-transform duration-150 bg-white"
-        >
-          <HiCog6Tooth className="text-lg text-neutral-400" />
-        </Link>
+        <div className="flex items-center gap-2">
+          <Link
+            href="/dashboard/portfolio"
+            className="border-2 p-1 rounded-full active:scale-90 transition-transform duration-150 bg-white"
+          >
+            <HiBriefcase className="text-lg text-neutral-400" />
+          </Link>
+          <Link
+            href="/dashboard/settings"
+            className="border-2 p-1 rounded-full active:scale-90 transition-transform duration-150 bg-white"
+          >
+            <HiCog6Tooth className="text-lg text-neutral-400" />
+          </Link>
+        </div>
       </div>
       <div className="flex-1 border-t-2 border-neutral-100 overflow-hidden">
         {children}
